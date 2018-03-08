58fcbe7bd8885cc46ecd89c300b1deadb5bab6d5
jiangbing <jiangbing17@jd.com>
2018-03-08 11:13:33 +0800

    spring 配置学习

---
+++ src/main/java/com/fish/config_learn/bean/English.java
@@ -0,0 +1,7 @@
+package com.fish.config_learn.bean;
+
+public class English implements Language {
+    public String getName() {
+        return "English";
+    }
+}
--- src/main/java/com/fish/config_learn/bean/Speak.java
+++ src/main/java/com/fish/config_learn/bean/Speak.java
@@ -1,18 +1,34 @@
 package com.fish.config_learn.bean;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
+import javax.annotation.Resource;
+
 public class Speak {
 
-    private Chinese chinese;
+    @Autowired
+    private Language chinese;
+
+    @Resource
+    private Language english;
 
     public String speakSomething() {
-        return chinese.getName();
+        return chinese.getName() + " and " + english.getName();
     }
 
-    public Chinese getChinese() {
-        return chinese;
-    }
+//    public Language getChinese() {
+//        return chinese;
+//    }
+//
+//    public Language getEnglish() {
+//        return english;
+//    }
 
-    public void setChinese(Chinese chinese) {
-        this.chinese = chinese;
-    }
+//    public void setEnglish(Language english) {
+//        this.english = english;
+//    }
+//
+//    public void setChinese(Language chinese) {
+//        this.chinese = chinese;
+//    }
 }
