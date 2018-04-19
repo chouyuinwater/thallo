59d7960ea08cfb15ccafe2005dace4dea3c6e5a2
koori <koorisan@hotmail.com>
2018-04-19 23:32:33 +0800

    追加内容

--- src/main/java/com/fish/config_learn/Main.java
+++ src/main/java/com/fish/config_learn/Main.java
@@ -4,11 +4,16 @@ import com.fish.config_learn.bean.Speak;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.io.FileInputStream;
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring/config_learn/spring-main.xml");
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("spring/spring-main.xml");
         Speak test = (Speak)ctx.getBean("speak");
         System.out.println(test.speakSomething());
+        ArrayList
+
     }
 }
