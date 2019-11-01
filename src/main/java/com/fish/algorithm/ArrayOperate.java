e7095bdeb4996007d7cc3ea8f48332f273d0cfa2
jiangbing <jiangbing17@jd.com>
2019-11-01 14:24:54 +0800

    双栈队列和双队列栈

--- src/main/java/com/fish/algorithm/ArrayOperate.java
+++ src/main/java/com/fish/algorithm/ArrayOperate.java
@@ -64,35 +64,35 @@ public class ArrayOperate {
      * 思路：在合并两个数组包括字符串时，如果从前往后复制每个数字或字符则需要重复移动数字或字符多次（O(n^2)）,
      * 可以考虑从后往前复制，这样就能减少移动次数，从而提高效率。
      *
-     * 总结：1. 在处理循环时可以考虑使用while来解决一些for循环解决不了的问题（本题中如果FOR循环数组A2则无法几角问题）
+     * 总结：1. 在处理循环时可以考虑使用while来解决一些for循环解决不了的问题（本题中如果FOR循环数组A2则无法解决问题）
      *       2. 循环解决数组问题可以考虑从后向前循环解决问题，减少数组移动。
      *       3. 在一个数组内进行插入可以考虑从后向前。
      */
     private static void orderedTwoDimensionalArrayInsertion() {
 //        int[] A1 = {1, 4, 6, 8, 9};
-        int[] A1 = new int[11];
-        A1[0] = 1;
-        A1[1] = 4;
-        A1[2] = 6;
-        A1[3] = 8;
-        A1[4] = 9;
-        int[] A2 = {1, 3, 7, 7, 9, 10};
+        int[] a1 = new int[11];
+        a1[0] = 1;
+        a1[1] = 4;
+        a1[2] = 6;
+        a1[3] = 8;
+        a1[4] = 9;
+        int[] a2 = {1, 3, 7, 7, 9, 10};
 
-        int end = 5 + A2.length - 1;
+        int end = 5 + a2.length - 1;
         int indexA1 = 4;
-        int indexA2 = A2.length - 1;
+        int indexA2 = a2.length - 1;
         while (indexA2 >= 0 && end >= 0 && indexA1>= 0) {
-            if (A2[indexA2] >= A1[indexA1]) {
-                A1[end] = A2[indexA2];
+            if (a2[indexA2] >= a1[indexA1]) {
+                a1[end] = a2[indexA2];
                 indexA2--;
             } else {
-                A1[end] = A1[indexA1];
+                a1[end] = a1[indexA1];
                 indexA1--;
             }
             end--;
         }
-        for (int i = 0; i < A1.length ; i++) {
-            System.out.println(A1[i]);
+        for (int i = 0; i < a1.length ; i++) {
+            System.out.println(a1[i]);
         }
     }
 
--- src/main/java/com/fish/algorithm/StackAndQueueOperate.java
+++ src/main/java/com/fish/algorithm/StackAndQueueOperate.java
@@ -21,6 +21,16 @@ public class StackAndQueueOperate {
 
     /**
      * 双栈实现队列
+     *
+     * 栈后进先出 LIFO 限定仅在表尾进行插入或删除操作的线性表。
+     * 也就是说它有两个操作,且操作数都在线性表尾部
+     * 队列先进先出 FIFO 是一种特殊的线性表，它只允许在表的前端（front）进行删除操作，
+     * 而在表的后端（rear）进行插入操作。
+     *
+     * 插入元素先放入Stack1， 需要先将保存在Stack2的元素按顺序转移会Stack1，再把新插入元素放入队列尾部
+     * 删除时把Stack1的元素（从后往前弹出元素）弹出到Stack2中，再弹出Stack2
+     *
+     * 两个瓶子互相倒水
      */
     private class TwoStackMakeOneQueue<T> {
         private Stack<T> stack1 = new Stack<>();
@@ -44,8 +54,33 @@ public class StackAndQueueOperate {
 
     /**
      * 双队列实现栈
+     *
+     * queue1 保存元素
+     * 删除时把元素放入另一个队列，将最后一个元素删除，本队列清空
      */
-    private class TwoQueueMakeStack {
+    private class TwoQueueMakeStack<T> {
+        private Queue<T> queue1 = new ArrayDeque<>();
+        private Queue<T> queue2 = new ArrayDeque<>();
 
+        public T push(T element) {
+            queue1.offer(element);
+            return element;
+        }
+
+        public T pop() {
+            if (queue1.size() > 0) {
+                while (queue1.size() > 1) {
+                    queue2.offer(queue1.poll());
+                }
+                return queue1.poll();
+            }
+            if (queue2.size() > 0) {
+                while (queue2.size() > 1) {
+                    queue1.offer(queue2.poll());
+                }
+                return queue2.poll();
+            }
+            return null;
+        }
     }
 }
