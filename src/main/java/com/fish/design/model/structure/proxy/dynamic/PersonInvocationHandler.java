0765bc7f772ba4765d2a345c802f608db026fa64
bingQ <koorisan@hotmail.com>
2019-11-03 23:05:34 +0800

    动态代理

--- src/main/java/com/fish/design/model/structure/proxy/dynamic/PersonInvocationHandler.java
+++ src/main/java/com/fish/design/model/structure/proxy/dynamic/PersonInvocationHandler.java
@@ -17,7 +17,7 @@ public class PersonInvocationHandler<T> implements InvocationHandler {
         System.out.println("被动态代理类回调执行, 代理类 proxyClass ="+proxy.getClass()+" 方法名: " + method.getName() + "方法. 方法返回类型："+method.getReturnType()
                 +" 接口方法入参数组: "+(args ==null ? "null" : Arrays.toString(args)));
         // 加入统一处理的代理逻辑
-        System.out.println("ProxySubject is HERE!");
+        System.out.println("Proxy is HERE!");
         method.invoke(target, args);
         System.out.println("POWER is HERE!");
 
